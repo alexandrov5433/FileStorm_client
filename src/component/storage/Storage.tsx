37feecd9e1f960ce3c6ed36e0f8f55f2ccfad81d
alexandrov5433 <alexandrov5433@gmail.com>
2025-05-28 23:00:30 +0200

    Add New dropdown styled.

--- src/component/storage/Storage.tsx
+++ src/component/storage/Storage.tsx
@@ -26,10 +26,20 @@ export default function Storage() {
                             Add New
                         </button>
 
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        <ul id="side-options-add-new-dropdown" className="dropdown-menu">
+                            <li>
+                                <span className="dropdown-item">
+                                    <i className="bi bi-folder2"></i>
+                                    Directory
+                                </span>
+                            </li>
+                            <div className="custom-horizontal-divider"></div>
+                            <li>
+                                <span className="dropdown-item">
+                                    <i className="bi bi-file-earmark-arrow-up"></i>
+                                    Upload File
+                                </span>
+                            </li>
                         </ul>
                     </div>
                     <button id="storageSideOptionsBtn" onClick={toggleSideOptionsDisplay} className="custom-btn secondary-btn" type="button">More</button>
@@ -38,7 +48,7 @@ export default function Storage() {
                         sideOptionsDisplayToggler={toggleSideOptionsDisplay} />
                 </section>
                 <section id="storageFileOverviewContainer" className="flex-col-strech-wrapper">
-                    <Outlet/>
+                    <Outlet />
                 </section>
             </div>
         </>
