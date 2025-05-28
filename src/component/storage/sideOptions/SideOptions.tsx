d9a39c354b35a2647f7eddbeec0149cdf4422ecb
alexandrov5433 <alexandrov5433@gmail.com>
2025-05-28 19:08:02 +0200

    Side pannel with options/navigation added.

--- src/component/storage/sideOptions/SideOptions.tsx
+++ src/component/storage/sideOptions/SideOptions.tsx
@@ -1,17 +1,77 @@
+import { NavLink } from 'react-router';
+import { useAppSelector } from '../../../lib/redux/reduxTypedHooks';
 import './sideOptions.sass';
+import { useRef } from 'react';
+import mouseOutClassUpdate from '../../../lib/hook/mouseOutClassUpdate';
 
 export default function SideOptions({
     sideOptionsDisplay,
     sideOptionsDisplayToggler
-} : {
+}: {
     sideOptionsDisplay: boolean,
     sideOptionsDisplayToggler: () => void
 }) {
+    const user = useAppSelector(state => state.user);
+
+    const myStorageRef = useRef<HTMLAnchorElement>(null);
+    const favoriteRef = useRef<HTMLAnchorElement>(null);
+    const mySharedFilesRef = useRef<HTMLAnchorElement>(null);
+    const sharedWithMeRef = useRef<HTMLAnchorElement>(null);
+    
+    mouseOutClassUpdate(myStorageRef.current!, 'mouseout', 0.3);
+    mouseOutClassUpdate(favoriteRef.current!, 'mouseout', 0.3);
+    mouseOutClassUpdate(mySharedFilesRef.current!, 'mouseout', 0.3);
+    mouseOutClassUpdate(sharedWithMeRef.current!, 'mouseout', 0.3);
 
     return (
         <div id="side-options-main-container" className={sideOptionsDisplay ? 'show' : ''}>
-            <h3>Username here</h3>
-            <button onClick={sideOptionsDisplayToggler}>Close</button>
+            <section id="side-options-username-container">
+                <h6>{user.username}</h6>
+                <button className="custom-icon-btn" onClick={sideOptionsDisplayToggler}>
+                    <i className="bi bi-x-lg"></i>
+                </button>
+            </section>
+            <div className="custom-horizontal-divider"></div>
+            <section id="side-options-location-container">
+                <ul>
+                    <li>
+                        <NavLink to="my-storage" ref={myStorageRef}>
+                            <span></span>
+                            <div>
+                                <i className="bi bi-archive"></i>
+                            </div>
+                            My Storage
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="favorite" ref={favoriteRef}>
+                            <span></span>
+                            <div>
+                                <i className="bi bi-heart"></i>
+                            </div>
+                            Favorite
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="my-shared-files" ref={mySharedFilesRef}>
+                            <span></span>
+                            <div>
+                                <i className="bi bi-share"></i>
+                            </div>
+                            My Shared Files
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="shared-with-me" ref={sharedWithMeRef} className="">
+                            <span></span>
+                            <div>
+                                <i className="bi bi-people"></i>
+                            </div>
+                            Shared With Me
+                        </NavLink>
+                    </li>
+                </ul>
+            </section>
         </div>
     );
 }
