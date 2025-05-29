35ec645789fb5d9a65ac1d3a1f127ebae328c544
alexandrov5433 <alexandrov5433@gmail.com>
2025-05-29 16:52:41 +0200

    Chaged cloud from imported svg to icon from BS for optimized performance.

--- src/component/skeleton/storageViewLoader/StorageViewLoader.tsx
+++ src/component/skeleton/storageViewLoader/StorageViewLoader.tsx
@@ -1,13 +1,11 @@
 import './storageViewLoader.sass';
 
-import cloud from '../../../assets/FileStorm_cloud.svg';
-
 export default function StorageViewLoader() {
     return (
         <div id="storage-view-loader-main-container" className="flex-col-strech-wrapper anime-fade-in">
             <section className="storage-view-loader-container">
                 <div className="cloud-container">
-                    <img src={cloud} alt="FileStorm cloud." />
+                    <i className="bi bi-cloud-fill"></i>
                 </div>
                 <div className="numbers-container">
                     <p className="storage-loader-1">1</p>
