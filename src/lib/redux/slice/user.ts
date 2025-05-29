4b179e1f7024cd2e08d14aca92f1064e72a88560
alexandrov5433 <alexandrov5433@gmail.com>
2025-05-29 16:31:14 +0200

    Slice update.

--- src/lib/redux/slice/user.ts
+++ src/lib/redux/slice/user.ts
@@ -5,19 +5,28 @@ export const userSlice = createSlice({
     name: 'user',
     initialState: {
         id: 0,
-        username: ''
+        username: '',
+        email: '',
+        max_storage_space: 0,
+        bytes_in_storage: 0
     } as User,
     reducers: {
-        setUser: (state, action: {payload: User, type: string}) => {            
+        setUser: (state, action: { payload: User, type: string }) => {
             state.id = action.payload.id;
             state.username = action.payload.username;
+            state.email = action.payload.email;
+            state.max_storage_space = action.payload.max_storage_space;
+            state.bytes_in_storage = action.payload.bytes_in_storage;
         },
         setGuest: (state) => {
-            state.id = 0;
-            state.username = '';
+            state.id = 0,
+            state.username = '',
+            state.email = '',
+            state.max_storage_space = 0,
+            state.bytes_in_storage = 0
         }
     }
 });
 
-export const { setUser, setGuest} = userSlice.actions;
+export const { setUser, setGuest } = userSlice.actions;
 export default userSlice.reducer;
