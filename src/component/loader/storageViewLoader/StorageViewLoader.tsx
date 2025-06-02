9655e86cc8a04c5ded6ce5260b869f02f3af97d0
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-02 19:27:39 +0200

    File remane and loader simplyfied in order to display it faster.

--- src/component/loader/storageViewLoader/StorageViewLoader.tsx
+++ src/component/loader/storageViewLoader/StorageViewLoader.tsx
@@ -2,12 +2,15 @@ import './storageViewLoader.sass';
 
 export default function StorageViewLoader() {
     return (
-        <div id="storage-view-loader-main-container" className="flex-col-strech-wrapper anime-fade-in">
+        <div id="storage-view-loader-main-container" className="flex-col-strech-wrapper">
             <section className="storage-view-loader-container">
                 <div className="cloud-container">
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
                     <i className="bi bi-cloud-fill"></i>
                 </div>
-                <div className="numbers-container">
+                {/* <div className="numbers-container">
                     <p className="storage-loader-1">1</p>
                     <p className="storage-loader-2">0</p>
                     <p className="storage-loader-3">1</p>
@@ -15,7 +18,7 @@ export default function StorageViewLoader() {
                 </div>
                 <div className="computer-container">
                     <i className="bi bi-laptop"></i>
-                </div>
+                </div> */}
             </section>
         </div>
     );
