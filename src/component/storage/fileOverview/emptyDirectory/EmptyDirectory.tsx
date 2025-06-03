a4ff7148051dd78198d1b62f77711d135ba5ec9a
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-03 08:28:59 +0200

    Empty directory element added.

---
+++ src/component/storage/fileOverview/emptyDirectory/EmptyDirectory.tsx
@@ -0,0 +1,10 @@
+import './emptyDirectory.sass';
+
+export default function EmptyDirectory() {
+    return (
+        <div id="empty-directory-main-container">
+            <i className="bi bi-folder2-open"></i>
+            <p>Empty directory.</p>
+        </div>
+    );
+}
