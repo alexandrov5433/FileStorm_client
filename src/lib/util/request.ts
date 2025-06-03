263568c1d0176bb7ba657e9a211f2a40fa54bdae
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-03 10:09:48 +0200

    Type update.

--- src/lib/util/request.ts
+++ src/lib/util/request.ts
@@ -5,7 +5,7 @@
  * @param requestParams An object of all request parameters to be attached to the request.
  * @returns The new url with the request parameters attached. If an empty object is supplied for requestParams the original base url is returned.
  */
-function addRequestParams(url: string, requestParams: { [key: string]: string }): string {
+function addRequestParams(url: string, requestParams: { [key: string]: string | number }): string {
     if (Object.keys(requestParams).length == 0) return url;
     let newUrl = `${url}?`;
     for (let [k, v] of Object.entries(requestParams)) {
