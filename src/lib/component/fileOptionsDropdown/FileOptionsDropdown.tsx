80bb555583178093e6b0ed0fdbcf93a104c23278
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-03 15:17:27 +0200

    Download option added in dropdown menu.

--- src/lib/component/fileOptionsDropdown/FileOptionsDropdown.tsx
+++ src/lib/component/fileOptionsDropdown/FileOptionsDropdown.tsx
@@ -1,12 +1,25 @@
+import type { Chunk } from '../../definition/chunk';
 import './fileOptionsDropdown.sass';
 
-export default function FileOptionsDropdown() {
+export default function FileOptionsDropdown({
+    chunk
+} : {
+    chunk: Chunk
+}) {
+
+    function download() {
+        const anchor = document.createElement('a');
+        anchor.href = `/api/file?fileId=${chunk.id}`;
+        anchor.download = chunk.name;
+        anchor.click();
+    }
+    
     return (
         <div className="dropdown file-options custom-icon-btn" data-bs-toggle="dropdown">
             <i className="bi bi-three-dots-vertical"></i>
             <ul className="dropdown-menu custom-dropdown">
                 <li>
-                    <span className="dropdown-item">
+                    <span className="dropdown-item" onClick={download}>
                         <i className="bi bi-download"></i>
                         Download
                     </span>
