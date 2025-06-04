9cd512d83e957b0e219fadc340c5215ac9bd8a83
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-04 12:05:12 +0200

    Favorite view added.

--- src/component/storage/view/favorite/Favorite.tsx
+++ src/component/storage/view/favorite/Favorite.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getFavoriteRequest } from '../../../../lib/action/favoriteRequest';
 import fetcher from '../../../../lib/action/fetcher';
 import './favorite.sass';
@@ -11,6 +11,10 @@ export default function Favorite() {
     const [favorite, setFavorite] = useState<Chunk[]>([]);
     const [isFavoriteLoading, setFavoriteLoading] = useState(true);
 
+    useEffect(() => {
+        getFavorite();
+    }, []);
+
     async function getFavorite() {
         setFavoriteLoading(true);
         const res = await fetcher(getFavoriteRequest());
@@ -22,10 +26,17 @@ export default function Favorite() {
 
     return (
         <div id="favorite-main-container" className="flex-col-strech-wrapper">
-            <h1>Favorite</h1>
+            <section id="favorite-top-bar">
+                <h3>Favorite</h3>
+            </section>
             {
-                isFavoriteLoading ? <StorageViewLoader/> :
-                <FileOverview/>
+                isFavoriteLoading ? <StorageViewLoader /> :
+                    <FileOverview
+                        displayEntities='filesOnly'
+                        hydratedChunkRefs={favorite}
+                        emptyDirectoryTextContent='No Favorites.'
+                        emptyDirectoryIcon='file'
+                    />
             }
         </div>
     );
