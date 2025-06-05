4b2c698ff9ca9db51411b62286e9a3dc14ddca7d
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-05 18:17:57 +0200

    Utility function added.

--- src/lib/util/directory.ts
+++ src/lib/util/directory.ts
@@ -8,6 +8,20 @@ function buildDirectoryPath(parts: Array<string|number>): string {
     return parts.join('/');
 }
 
+/**
+ * Compares (!=) the elements of two arrays without using coercion.
+ * @param arrOne The first array to compare against the second.
+ * @param arrTwo The second array to compare against the first.
+ * @returns true if all elements on all indexes are equal, otherwise false.
+ */
+function compareArrays(arrOne: any[], arrTwo: any[]) {
+    for (let i = 0; i < arrOne.length; i++) {
+        if (arrOne[i] != arrTwo[i]) return false;
+    }
+    return true;
+}
+
 export {
-    buildDirectoryPath
+    buildDirectoryPath,
+    compareArrays
 };
