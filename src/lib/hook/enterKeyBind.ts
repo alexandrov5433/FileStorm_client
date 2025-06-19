b6f1dcc682e39c6cb2bea0c2c8e4c97e322b969f
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-19 16:38:36 +0200

    Hook updated with element presence check.

--- src/lib/hook/enterKeyBind.ts
+++ src/lib/hook/enterKeyBind.ts
@@ -6,6 +6,7 @@ import { useEffect } from "react";
  */
 export default function enterKeyBind(element: HTMLElement) {
     useEffect(() => {
+        if (!element) return;
         const keyStrokeListener = (e: KeyboardEvent) => {
             if (e.key == 'Enter') {
                 element.click();
