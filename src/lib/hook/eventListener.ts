315c8dfb42e461f10bf5a96bf142b701daf316e1
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-20 16:47:28 +0200

    Hooks for adding event listeners added.

---
+++ src/lib/hook/eventListener.ts
@@ -0,0 +1,45 @@
+import { useEffect, useRef, type RefObject } from "react";
+
+type ExtendedEventMap = DocumentEventMap & WindowEventMap & HTMLElementEventMap;
+
+export function useEventListener<K extends keyof ExtendedEventMap>(
+    element: HTMLElement | Window | Document,
+    eventType: K,
+    listener: (event: ExtendedEventMap[K]) => any | void
+) {
+    const eventListenerRef = useRef(listener);
+
+    useEffect(() => {
+        eventListenerRef.current = listener;
+    }, [listener]);
+
+    useEffect(() => {
+        if (!element || !eventType) return;
+        const _listener = (e: Event) => eventListenerRef.current(e as ExtendedEventMap[K]);
+        element.addEventListener(eventType, _listener);
+        return () => element.removeEventListener(eventType, _listener);
+    }, [element, eventType]);
+}
+
+export function useEventListenerForRef<K extends keyof ExtendedEventMap>(
+    ref: RefObject<HTMLElement>,
+    eventType: K,
+    listener: (event: ExtendedEventMap[K]) => any | void
+) {
+    const eventListenerRef = useRef(listener);
+
+    useEffect(() => {
+        eventListenerRef.current = listener;
+    }, [listener]);
+
+    useEffect(() => {
+        if (!ref || !ref.current || !eventType) return;
+        const _listener = (e: Event) => eventListenerRef.current(e as ExtendedEventMap[K]);
+        ref.current.addEventListener(eventType, _listener);
+        return () => {
+            if (!ref || !ref.current) return;
+            // when a DOM Element is deleted from the DOM, event listeners attached to it are also removed, as long as there are no references to that same element.
+            ref.current?.removeEventListener(eventType, _listener)
+        };
+    }, [ref, eventType]);
+}
