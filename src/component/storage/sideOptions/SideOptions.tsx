991abaa906729588b5966d97151de3b8d1b45e57
alexandrov5433 <alexandrov5433@gmail.com>
2025-07-02 08:47:01 +0200

    Storage capacity information added.

--- src/component/storage/sideOptions/SideOptions.tsx
+++ src/component/storage/sideOptions/SideOptions.tsx
@@ -1,10 +1,11 @@
 import { NavLink } from 'react-router';
 import { useAppDispatch, useAppSelector } from '../../../lib/redux/reduxTypedHooks';
 import './sideOptions.sass';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import mouseOutClassUpdate from '../../../lib/hook/mouseOutClassUpdate';
 import { setDirPath } from '../../../lib/redux/slice/directory';
 import { clearCheckedList } from '../../../lib/redux/slice/checkedEntities';
+import { bytesToGigabytes } from '../../../lib/util/convert';
 
 export default function SideOptions({
     sideOptionsDisplay,
@@ -16,15 +17,31 @@ export default function SideOptions({
     const dispatch = useAppDispatch();
     const user = useAppSelector(state => state.user);
 
+    const [valBytesInStorage, setValBytesInStorage] = useState('0');
+    const [valMaxStorageSpace, setValMaxStorageSpace] = useState('0');
+
     const myStorageRef = useRef<HTMLAnchorElement>(null);
     const favoriteRef = useRef<HTMLAnchorElement>(null);
     const mySharedFilesRef = useRef<HTMLAnchorElement>(null);
     const sharedWithMeRef = useRef<HTMLAnchorElement>(null);
+    const storageSpaceBarFillerRef = useRef<HTMLDivElement>(null);
 
     mouseOutClassUpdate(myStorageRef.current!, 'mouseout', 0.3);
     mouseOutClassUpdate(favoriteRef.current!, 'mouseout', 0.3);
     mouseOutClassUpdate(mySharedFilesRef.current!, 'mouseout', 0.3);
     mouseOutClassUpdate(sharedWithMeRef.current!, 'mouseout', 0.3);
+
+    useEffect(() => {
+        if (!user) return;
+        const storageBar = storageSpaceBarFillerRef.current;
+        if (!storageBar) return;
+        setValBytesInStorage(bytesToGigabytes(user?.bytesInStorage || 0, 2));
+        setValMaxStorageSpace(bytesToGigabytes(user?.maxStorageSpace || 0, 0));
+        let percent = (user.bytesInStorage / user.maxStorageSpace) * 100;
+        storageBar!.style.width = `${percent}%`;
+        console.log(user);
+        
+    }, [user]);
 
     return (
         <div id="side-options-main-container" className={sideOptionsDisplay ? 'show' : ''}>
@@ -71,6 +88,13 @@ export default function SideOptions({
                         </NavLink>
                     </li>
                 </ul>
+            </section>
+            <div className="custom-horizontal-divider"></div>
+            <section id="side-options-storage-space-container">
+                <div className="storage-space-bar">
+                    <div ref={storageSpaceBarFillerRef} className="storage-space-bar-filler"></div>
+                </div>
+                <p className="storage-space-numbers">{valBytesInStorage} GB of {valMaxStorageSpace} GB used</p>
             </section>
         </div>
     );
