68cc70d56d3267a68998a90aaa50073082363ac4
alexandrov5433 <alexandrov5433@gmail.com>
2025-07-02 08:48:54 +0200

    Error message added to the returned object.

--- src/lib/action/fileUpload.ts
+++ src/lib/action/fileUpload.ts
@@ -25,6 +25,7 @@ export default async function fileUpload(
             xml.send(body);
         } catch (e) {
             ret.status = 0;
+            ret.msg = (e as Error).message;
             ret.error = e as Error;
             ret.payload = null;
             reject(ret);
