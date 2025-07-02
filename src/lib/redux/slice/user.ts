cdcd55eb9d1d6ee425b4b018ce5aa79544a8ebe4
alexandrov5433 <alexandrov5433@gmail.com>
2025-07-02 08:47:56 +0200

    Storage capacity related reducers added.

--- src/lib/redux/slice/user.ts
+++ src/lib/redux/slice/user.ts
@@ -10,9 +10,32 @@ export const userSlice = createSlice({
         },
         setGuest: (_state) => {
             return null;
+        },
+        addBytesInStorage: (state, action: { payload: number, type: string }) => {
+            if (!state) return state;
+            return {
+                ...state,
+                bytesInStorage: (state?.bytesInStorage || 0) + action.payload
+            }
+        },
+        removeBytesInStorage: (state, action: { payload: number, type: string }) => {
+            if (!state) return state;
+            let newVal = (state?.bytesInStorage || 0) - action.payload;
+            if (newVal < 0) {
+                newVal = 0;
+            }
+            return {
+                ...state,
+                bytesInStorage: newVal
+            }
         }
     }
 });
 
-export const { setUser, setGuest } = userSlice.actions;
+export const {
+    setUser,
+    setGuest,
+    addBytesInStorage,
+    removeBytesInStorage
+} = userSlice.actions;
 export default userSlice.reducer;
