77dca5b8ea4b2561a68308feecaafb8060a856e0
alexandrov5433 <alexandrov5433@gmail.com>
2025-07-02 08:48:08 +0200

    Utility function added.

---
+++ src/lib/util/convert.ts
@@ -0,0 +1,13 @@
+/**
+ * Contertes the given bytes in gigabytes.
+ * @param bytes Bytes to convert.
+ * @param toFixed Number of digits after the decimal point. Method used: Number.toFixed() 
+ * @returns The string representation of the converted result.
+ */
+function bytesToGigabytes(bytes: number, toFixed: number) {
+    return (bytes / 1024 / 1024 / 1024).toFixed(toFixed);
+}
+
+export {
+    bytesToGigabytes
+};
