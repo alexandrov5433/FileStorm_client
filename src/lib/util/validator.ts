6313a644239811510d1ac01b7787e9de005c5ac7
alexandrov5433 <alexandrov5433@gmail.com>
2025-07-04 17:23:10 +0200

    Cleanup.

--- src/lib/util/validator.ts
+++ src/lib/util/validator.ts
@@ -5,9 +5,7 @@
  * @returns True if the value is valid, false otherwise.
  */
 function validateFileAndDirName(value: string) {
-    console.log(value);
     if (!value) return false;
-    // return !value.match('\\\\|/|:|\\*|\\?|\"|<|>|\\|');
     return !new RegExp('\\\\|/|:|\\*|\\?|\"|<|>|\\|').test(value);
 }
 
