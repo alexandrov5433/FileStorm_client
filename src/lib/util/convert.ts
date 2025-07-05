12f066e2a46a945095d2e21c0dda20f2e87b9627
alexandrov5433 <alexandrov5433@gmail.com>
2025-07-05 17:14:23 +0200

    Conversion function added.

--- src/lib/util/convert.ts
+++ src/lib/util/convert.ts
@@ -8,6 +8,17 @@ function bytesToGigabytes(bytes: number, toFixed: number) {
     return (bytes / 1024 / 1024 / 1024).toFixed(toFixed);
 }
 
+/**
+ * Contertes the given bytes in megabytes.
+ * @param bytes Bytes to convert.
+ * @param toFixed Number of digits after the decimal point. Method used: Number.toFixed() 
+ * @returns The string representation of the converted result.
+ */
+function bytesToMegabytes(bytes: number, toFixed: number) {
+    return (bytes / 1024 / 1024).toFixed(toFixed);
+}
+
 export {
-    bytesToGigabytes
+    bytesToGigabytes,
+    bytesToMegabytes
 };
